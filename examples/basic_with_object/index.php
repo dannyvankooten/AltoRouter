2e7c98556d01c038d21cdfca8d865000676499b6
Lukas Kämmerling <admin@myonepager.net>
2015-07-13 18:39:40 +0200

    Added possibility for return of an AltoRouterEntity Object

---
+++ examples/basic_with_object/index.php
@@ -0,0 +1,27 @@
+<?php
+
+require '../../AltoRouter.php';
+define("ALTOROUTER_RETURN_OBJECT", true); // Let`s say the AltoRouter that must return an Object
+$router = new AltoRouter();
+$router->setBasePath('/AltoRouter/examples/basic');
+$router->map('GET|POST','/', 'home#index', 'home');
+$router->map('GET','/users/', array('c' => 'UserController', 'a' => 'ListAction'));
+$router->map('GET','/users/[i:id]', 'users#show', 'users_show');
+$router->map('POST','/users/[i:id]/[delete|update:action]', 'usersController#doAction', 'users_do');
+
+// match current request
+$match = $router->match();
+?>
+<h1>AltoRouter</h1>
+
+<h3>Current request: </h3>
+<pre>
+	Target: <?php var_dump($match->target); ?>
+	Params: <?php var_dump($match->params); ?>
+	Name: 	<?php var_dump($match->name); ?>
+</pre>
+
+<h3>Try these requests: </h3>
+<p><a href="<?php echo $router->generate('home'); ?>">GET <?php echo $router->generate('home'); ?></a></p>
+<p><a href="<?php echo $router->generate('users_show', array('id' => 5)); ?>">GET <?php echo $router->generate('users_show', array('id' => 5)); ?></a></p>
+<p><form action="<?php echo $router->generate('users_do', array('id' => 10, 'action' => 'update')); ?>" method="post"><button type="submit"><?php echo $router->generate('users_do', array('id' => 10, 'action' => 'update')); ?></button></form></p>
